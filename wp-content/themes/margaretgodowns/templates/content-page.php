e0e08b5c54dfd461333fbfa4209f06bc75e57189
Margaret Godowns <margaret@fuzzco.com>
2016-11-01 21:43:36 -0400

    rest of it

--- wp-content/themes/margaretgodowns/templates/content-page.php
+++ wp-content/themes/margaretgodowns/templates/content-page.php
@@ -1,14 +1,14 @@
 <div class="intro">
-  <div class="contact-info line-left">
+  <div class="contact-info line-left wow fadeInUp">
     <?php the_content(); ?>
   </div>
-  <div class="image line-center">
+  <div class="image line-center wow fadeInUp">
     <img src="<?php the_post_thumbnail_url(); ?>">
     <small>Photo by Ben Jack of <a href="http://fuzzco.com">Fuzzco</a></small>
   </div>
 </div>
-<div class="featured-work-title">
-  <h3 class="line-right">Featured Work</h3>
+<div class="featured-work-title wow fadeIn">
+  <h3 class="line-right">Recent Work</h3>
 </div>
 <div class="featured-posts">
   <?php
@@ -22,7 +22,7 @@
     $loop = new WP_Query( $args );
     while ( $loop->have_posts() ) : $loop->the_post();
   ?>
-  <div class="flex-item">
+  <div class="flex-item wow fadeInUp">
     <a href="<?php echo the_field('url'); ?>" target="_blank">
       <h4><?php the_title(); ?></h4>
     <?php the_content(); ?>
@@ -30,4 +30,68 @@
   </div>
   <?php endwhile; ?>
 </div>
-
+<div class="catalogue-title wow fadeInUp">
+  <h3 class="line-left">Catalogue</h3>
+</div>
+<div class="catalogue">
+  <div class="wordpress-themes flex-item line-right">
+    <h5 class="wow fadeInUp">Wordpress Themes</h5>
+    <?php
+      $args = array(
+          'post_type'    => array(
+              'wordpress_theme'
+          ),
+          'posts_per_page' => -1,
+          'orderby' => 'menu_order'
+       );
+      $loop = new WP_Query( $args );
+      while ( $loop->have_posts() ) : $loop->the_post();
+    ?>
+      <div class="wow fadeInUp">
+        <a href="<?php echo the_field('url'); ?>" target="_blank">
+          <p><?php the_title(); ?></p>
+        </a>
+      </div>
+    <?php endwhile; ?>
+    <div class="ecommerce">
+      <h5 class="ecommerce-title wow fadeInUp">Ecommerce</h5>
+      <?php
+        $args = array(
+            'post_type'    => array(
+                'ecommerce'
+            ),
+            'posts_per_page' => -1,
+            'orderby' => 'menu_order'
+         );
+        $loop = new WP_Query( $args );
+        while ( $loop->have_posts() ) : $loop->the_post();
+      ?>
+        <div class="wow fadeInUp">
+          <a href="<?php echo the_field('url'); ?>" target="_blank">
+            <p><?php the_title(); ?></p>
+          </a>
+        </div>
+      <?php endwhile; ?>
+    </div>
+  </div>
+  <div class="wordpress-themes flex-item line-center">
+    <h5 class="wow fadeInUp">Feature Development &amp; Maintenance</h5>
+    <?php
+      $args = array(
+          'post_type'    => array(
+              'maintenance'
+          ),
+          'posts_per_page' => -1,
+          'orderby' => 'menu_order'
+       );
+      $loop = new WP_Query( $args );
+      while ( $loop->have_posts() ) : $loop->the_post();
+    ?>
+      <div class="wow fadeInUp">
+        <a href="<?php echo the_field('url'); ?>" target="_blank">
+          <p><?php the_title(); ?></p>
+        </a>
+      </div>
+    <?php endwhile; ?>
+  </div>
+</div>
--- wp-content/themes/margaretgodowns/templates/footer.php
+++ wp-content/themes/margaretgodowns/templates/footer.php
@@ -1,5 +1,17 @@
 <footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+  <div class="container footer">
+    <div class="flex-item wow fadeInUp">
+      <ul>
+        <li><a href="mailto:margaretgodowns@gmail.com">email</a></li>
+        <li><a href="https://www.instagram.com/margaretgodowns/" target="_blank">instagram</a></li>
+        <li><a href="https://github.com/margaretgodowns" target="_blank">github</a></li>
+      </ul>
+    </div>
+    <div class="flex-item wow fadeInUp">
+      <p>All items designed and developed with Fuzzco unless noted.</p>
+    </div>
+    <div class="flex-item wow fadeInUp">
+      <p>&copy; 2016</p>
+    </div>
   </div>
 </footer>
